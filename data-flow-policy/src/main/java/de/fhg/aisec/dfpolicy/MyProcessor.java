d82537387f9b882d716b965ef541a6097148198f
morbitze <mathias.morbitzer@aisec.fraunhofer.de>
2016-09-02 15:26:07 +0200

    Added forwarding of messages based on allow-rules

--- data-flow-policy/src/main/java/de/fhg/aisec/dfpolicy/MyProcessor.java
+++ data-flow-policy/src/main/java/de/fhg/aisec/dfpolicy/MyProcessor.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Properties;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.management.InstrumentationProcessor;
+import org.apache.camel.processor.SendProcessor;
 import org.apache.camel.AsyncCallback;
 import org.apache.camel.AsyncProcessor;
 import org.slf4j.Logger;
@@ -113,10 +115,62 @@ public class MyProcessor implements AsyncProcessor {
 
 	public void process(Exchange exchange) throws Exception {
 		
+		InstrumentationProcessor instrumentationprocessor;
+		SendProcessor sendprocessor;
+		String destination = null;
+		String labels_value; 
+		String label;
 		//label the new message if needed
 		exchange = LabelingProcess(exchange);
 
+		if (exchange.getProperty("labels") != null ) {
+			labels_value = exchange.getProperty("labels").toString();
+		} else {
+			labels_value = "";
+		}
+		
+		//figuring out where the message should go to
+		if (target instanceof InstrumentationProcessor) {
+			instrumentationprocessor = (InstrumentationProcessor) target;
+			if (instrumentationprocessor.getProcessor() instanceof SendProcessor) {
+				sendprocessor = (SendProcessor) instrumentationprocessor.getProcessor();
+				destination = sendprocessor.getEndpoint().getEndpointUri();
+			} else {
+				LOG.error("target is not an instance of SendProcessor");
+				return;
+			}
+		} else {
+			LOG.error("target is not an instance of InstrumentactionProcessor");
+			return;
+		}
+		
+		for (AllowRule allow_rule : allow_rules) {
+					
+			if (allow_rule.getDestination().equals(destination)){
+				
+				label = allow_rule.getLabel();
+				
+				//is the label contained in the labels-property, either as 'label', 'label,' or ',label'?
+				if (check_if_label_exists(label, labels_value)) {
+					
+					System.out.println("Found matching rule for destination " + destination +" with labels '" + labels_value + "': " + allow_rule.toString());
+					target.process(exchange);
+				}
+			}  
+		}
+		
     }
+	
+	public boolean check_if_label_exists(String label, String labels_value){
+		if (labels_value != "" 
+				&& labels_value.equals(label) 
+				|| labels_value.contains(label + ",") 
+				|| labels_value.contains("," + label)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 	public Exchange LabelingProcess(Exchange exchange) {
 		String from = exchange.getFromEndpoint().getEndpointUri();
@@ -133,13 +187,17 @@ public class MyProcessor implements AsyncProcessor {
 		for (LabelRule rule : label_rules) {
 			
 			//If we find a matching rule, we check if the labels-property already exists. If it does, we append our label, if not, we create the labels-property
-			if (rule.getSource().equals(from)) {
-				label = rule.getLabel();
-				labels_value = labels_value + label;
-				System.out.println("Got a message from " + from + ", will label it with " + label);
+			label = rule.getLabel();
+			
+			if (rule.getSource().equals(from) && !(check_if_label_exists(label, labels_value))) {
+
+				if (labels_value == "") {
+					labels_value = label;
+				} else {
+					labels_value = labels_value + "," + label;
+				}
+				System.out.println("Got a message from " + from + ", will label it with '" + label + "'");
 				exchange.setProperty("labels", labels_value);
-			}else {
-				System.out.println("Rule for "+rule.getSource()+" does not matching uri " +from+", ignoring...");
 			}
 		}
 		
