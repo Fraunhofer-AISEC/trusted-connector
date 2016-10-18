e412a63b1174acd629838d3d7211b28299772fe4
Gerd Brost <gerd.brost@aisec.fraunhofer.de>
2016-10-18 14:54:34 +0200

    now importing pkcs12 file and ca cert into keystore

--- openssl_cert_generation/import_ca_chain_into_keystore.sh
+++ openssl_cert_generation/import_ca_chain_into_keystore.sh
@@ -2,7 +2,15 @@
 #
 # Imports CA chain and connector cert into karaf keystore. Execute in $(Karaf-dir)/etc/keystores/
 # Then, build karaf. The keystore is automatically deployed.
+if [ $# -eq 3 ] ; then
+  echo "Three arguments supplied, which is good"
+else
+  echo "Invalid number of arguments: $# (expected 3)"
+  echo "Usage: $0 PKCS12_filename ca_cert_filename keystore_filename"
+  exit 1
+fi
+#keytool -keystore my-keystore.jks -storepass password -importcert -trustcacerts -noprompt -alias root -file rootca.cert
+keytool -keystore $3 -importcert -alias ca -file $2
+#keytool -keystore my-keystore.jks -storepass password -importcert -alias server -file connector.cert
+keytool -v -importkeystore -srckeystore $1 -srcstoretype PKCS12 -destkeystore $3 -deststoretype JKS
 
-keytool -keystore my-keystore.jks -storepass password -importcert -trustcacerts -noprompt -alias root -file rootca.cert
-keytool -keystore my-keystore.jks -storepass password -importcert -alias ca -file subca.cert
-keytool -keystore my-keystore.jks -storepass password -importcert -alias server -file connector.cert
