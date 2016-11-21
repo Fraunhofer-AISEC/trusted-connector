6fb22a9721b699589a5aab76f023abf4ecc69c2a
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2016-11-21 12:18:33 +0100

    Webconsole API: Using Optionals, added some endpoints for Apache Camel

--- ids-webconsole/src/main/java/de/fhg/aisec/ids/webconsole/api/AppApi.java
+++ ids-webconsole/src/main/java/de/fhg/aisec/ids/webconsole/api/AppApi.java
@@ -1,12 +1,19 @@
 package de.fhg.aisec.ids.webconsole.api;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import de.fhg.aisec.ids.api.cm.ApplicationContainer;
 import de.fhg.aisec.ids.api.cm.ContainerManager;
 import de.fhg.aisec.ids.webconsole.WebConsoleComponent;
 
@@ -25,47 +32,74 @@ public class AppApi {
 	@Path("list")
 	@Produces("application/json")
 	public String list() {
-		ContainerManager cml = WebConsoleComponent.getContainerManager();
-		if (cml==null) {
-			return new Gson().toJson("false");
+		List<ApplicationContainer> result = new ArrayList<>();
+		
+		Optional<ContainerManager> cml = WebConsoleComponent.getContainerManager();
+		if (cml.isPresent()) {
+			result = cml.get().list(false);
 		}
 		
-		return new Gson().toJson(cml.list(false));		
+		return new GsonBuilder().create().toJson(result);
 	}
 	
-	@GET
+	@POST
 	@Path("pull")
 	@Produces("application/json")
 	public String pull(@QueryParam("imageId") String imageId) {
-		ContainerManager cml = WebConsoleComponent.getContainerManager();
-		cml.pullImage(imageId);
-		return new Gson().toJson("OK");
+		Optional<ContainerManager> cml = WebConsoleComponent.getContainerManager();
+		
+		if (!cml.isPresent()) {
+			return new Gson().toJson("false");
+		}
+
+		new Thread() {
+			@Override
+			public void run() {
+				cml.get().pullImage(imageId);		
+			}
+		}.start();			
+		return new Gson().toJson("true");
 	}
 	
 	@GET
 	@Path("start")
 	@Produces("application/json")
 	public String start(@QueryParam("containerId") String containerId) {
-		ContainerManager cml = WebConsoleComponent.getContainerManager();
-		cml.startContainer(containerId);
-		return new Gson().toJson("OK");
+		Optional<ContainerManager> cml = WebConsoleComponent.getContainerManager();
+		
+		if (!cml.isPresent()) {
+			return new Gson().toJson("false");
+		}
+
+		cml.get().startContainer(containerId);
+		return new Gson().toJson("true");
 	}
 
 	@GET
 	@Path("stop")
 	@Produces("application/json")
 	public String stop(@QueryParam("containerId") String containerId) {
-		ContainerManager cml = WebConsoleComponent.getContainerManager();
-		cml.stopContainer(containerId);
-		return new Gson().toJson("OK");
+		Optional<ContainerManager> cml = WebConsoleComponent.getContainerManager();
+		
+		if (!cml.isPresent()) {
+			return new Gson().toJson("false");
+		}
+
+		cml.get().stopContainer(containerId);
+		return new Gson().toJson("true");
 	}
 
 	@GET
 	@Path("wipe")
 	@Produces("application/json")
 	public String wipe(@QueryParam("containerId") String containerId) {
-		ContainerManager cml = WebConsoleComponent.getContainerManager();
-		cml.wipe(containerId);
-		return new Gson().toJson("OK");
+		Optional<ContainerManager> cml = WebConsoleComponent.getContainerManager();
+
+		if (!cml.isPresent()) {
+			return new Gson().toJson("false");
+		}
+
+		cml.get().wipe(containerId);
+		return new Gson().toJson("true");
 	}
 }
