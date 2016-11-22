c8fa043f450b8d592f4aa648c194a3702eb13eac
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2016-11-22 10:08:52 +0100

    Notizen aus integration meeting

--- ids-container-manager/src/main/java/de/fhg/aisec/ids/cm/impl/trustx/TrustXCM.java
+++ ids-container-manager/src/main/java/de/fhg/aisec/ids/cm/impl/trustx/TrustXCM.java
@@ -12,6 +12,10 @@ import de.fhg.aisec.ids.api.cm.Protocol;
 /**
  * ContainerManager implementation for trust-x containers.
  * 
+ * /dev/socket/cml-control
+ * Protobuf: control.proto
+ * container.proto für container configs
+ *
  * @author Julian Schütte (julian.schuette@aisec.fraunhofer.de)
  *
  */
@@ -75,6 +79,12 @@ public class TrustXCM implements ContainerManager {
 			Protocol protocol, Decision decision) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public String getCml() {
+		// TODO Auto-generated method stub
+		return "trust-X";
 	}
 
 }
