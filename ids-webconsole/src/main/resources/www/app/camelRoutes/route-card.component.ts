69166599fc39a157d9bbf8602c3d6c158c8879de
Hamed Rasifard <hamed.rasifard@aisec.fraunhofer.de>
2016-12-16 13:19:34 +0100

    Style camel-routes page

--- ids-webconsole/src/main/resources/www/app/camelRoutes/route-card.component.ts
+++ ids-webconsole/src/main/resources/www/app/camelRoutes/route-card.component.ts
@@ -11,22 +11,23 @@ import { CamelRoutesService } from './camelRoutes.service';
         <h2 class="mdl-card__title-text">{{route.id}}</h2>
       </div>
       <div class="mdl-card__supporting-text">
-        Description: {{route.description}}
+        <div class="col-3">  Description: </div> <div class="col-9">{{route.description}}</div>
         <div class="mdl-grid">
           <div class="mdl-cell mdl-cell--4-col">
             Trust
-          </div>          
+          </div>
         </div>
-        Uptime: {{route.uptime}}<br />
-        Context: {{route.context}}<br />
-        Visualization: {{route.dot}}<br />
-        Status: {{route.status}}<br />
+        <div class="col-3">Uptime:</div> <div class="col-9">{{route.uptime}}</div>
+        <div class="col-3">Context:</div> <div class="col-9">{{route.context}}</div>
+        <div class="col-3">Visualization:</div> <div class="col-9">{{route.dot}}</div>
+        <div class="col-3">Status:</div> <div class="col-9">{{route.status}}</div>
       </div>
       <div class="mdl-card__actions mdl-card--border">
           <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"><i class="material-icons" role="presentation">start</i></a>
           <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"><i class="material-icons" role="presentation">pause</i></a>
           <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"><i class="material-icons" role="presentation">delete</i></a>
-      </div>`
+      </div>`,
+      styleUrls: ['app/camelRoutes/route-card.component.css']
 })
 export class RouteCardComponent implements OnInit {
   @Input() route: Route;
