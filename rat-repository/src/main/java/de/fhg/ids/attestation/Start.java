93fbb89b30af79d5fddba9e006fdca597e7987b0
Georg Räß <georg.raess@aisec.fraunhofer.de>
2016-12-16 10:10:13 +0100

    execution of rat-repository before unit test via process-exec-maven-plugin

--- rat-repository/src/main/java/de/fhg/ids/attestation/Start.java
+++ rat-repository/src/main/java/de/fhg/ids/attestation/Start.java
@@ -4,7 +4,7 @@ public class Start {
     private static RemoteAttestationServer ratServer;
 
 	public static void main(String[] args) throws Exception {
-		ratServer = new RemoteAttestationServer("127.0.0.1" , "configurations/check", Integer.parseInt(args[0]));
+		ratServer = new RemoteAttestationServer("127.0.0.1" , "configurations/check", 31337);
         try {
         	ratServer.start();
         	ratServer.join();
