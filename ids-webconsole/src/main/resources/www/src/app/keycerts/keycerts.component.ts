2c9dd7d4f08fadd82bafa9c063e07ab33a422670
Gerd Brost <gerd.brost@aisec.fraunhofer.de>
2017-01-30 15:24:38 +0100

    fixed node js files

--- ids-webconsole/src/main/resources/www/src/app/keycerts/keycerts.component.ts
+++ ids-webconsole/src/main/resources/www/src/app/keycerts/keycerts.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'keycerts',
-  templateUrl: 'app/keycerts/keycerts.component.html'
+  templateUrl: 'keycerts.component.html'
 })
 export class KeycertsComponent {
   constructor() {}
