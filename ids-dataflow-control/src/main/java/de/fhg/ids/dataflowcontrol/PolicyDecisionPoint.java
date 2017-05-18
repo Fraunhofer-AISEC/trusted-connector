6512a6ee08a8be54f7971e723758c96196adb795
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2017-05-18 09:43:45 +0200

    Prolog integrated into PDP

--- ids-dataflow-control/src/main/java/de/fhg/ids/dataflowcontrol/PolicyDecisionPoint.java
+++ ids-dataflow-control/src/main/java/de/fhg/ids/dataflowcontrol/PolicyDecisionPoint.java
@@ -1,16 +1,40 @@
 package de.fhg.ids.dataflowcontrol;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import alice.tuprolog.InvalidTheoryException;
 import de.fhg.aisec.ids.api.policy.DecisionRequest;
+import de.fhg.aisec.ids.api.policy.PAP;
 import de.fhg.aisec.ids.api.policy.PDP;
 import de.fhg.aisec.ids.api.policy.PolicyDecision;
+import de.fhg.ids.dataflowcontrol.lucon.LuconEngine;
 
-@Component(enabled=true, immediate=true, name="ids-dataflow-control")
-public class PolicyDecisionPoint implements PDP {
+/**
+ * servicefactory=false is the default and actually not required. But we want to make
+ * clear that this is a singleton, i.e. there will only be one instance of
+ * PolicyDecisionPoint within the whole runtime.
+ * 
+ * @author Julian Schuette (julian.schuette@aisec.fraunhofer.de)
+ *
+ */
+@Component(enabled = true, immediate = true, name = "ids-dataflow-control", servicefactory = false)
+public class PolicyDecisionPoint implements PDP, PAP {
 	private static final Logger LOG = LoggerFactory.getLogger(PolicyDecisionPoint.class);
+	private LuconEngine engine;
+	
+	@Activate
+	public void activate(ComponentContext ctx) {
+		if (this.engine == null) {
+			this.engine = new LuconEngine(System.out);
+		}
+	}
 
 	@Override
 	public PolicyDecision requestDecision(DecisionRequest req) {
@@ -18,11 +42,31 @@ public class PolicyDecisionPoint implements PDP {
 		PolicyDecision dec = new PolicyDecision();
 		dec.setDecision(PolicyDecision.Decision.ALLOW);
 		dec.setReason("Dummy");
+// TODO Call Prolog here
+//		try {
+//			this.engine.query("rule(_).", false);
+//		} catch (NoMoreSolutionException | MalformedGoalException | NoSolutionException e) {
+//			e.printStackTrace();
+//		}
 		return dec;
 	}
 
 	@Override
 	public void clearAllCaches() {
-		// Nothing to do here at the moment		
+		// Nothing to do here at the moment
+	}
+
+	@Override
+	public void loadPolicy(InputStream is) {
+		try {
+			this.engine.loadPolicy(is);
+		} catch (InvalidTheoryException | IOException e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public String getPolicy() {
+		return this.engine.getTheory();
 	}
 }
---
+++ ids-dataflow-control/src/main/java/de/fhg/ids/dataflowcontrol/lucon/LuconEngine.java
@@ -0,0 +1,82 @@
+package de.fhg.ids.dataflowcontrol.lucon;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import alice.tuprolog.InvalidTheoryException;
+import alice.tuprolog.MalformedGoalException;
+import alice.tuprolog.NoMoreSolutionException;
+import alice.tuprolog.NoSolutionException;
+import alice.tuprolog.Prolog;
+import alice.tuprolog.SolveInfo;
+import alice.tuprolog.Theory;
+
+/**
+ * LUCON (Logic based Usage Control) policy decision engine.
+ * 
+ * This engine uses tuProlog as a logic language implementation to answer policy
+ * decision requests.
+ * 
+ * @author Julian Schuette (julian.schuette@aisec.fraunhofer.de)
+ *
+ */
+public class LuconEngine {
+	private static final Logger LOG = LoggerFactory.getLogger(LuconEngine.class);
+	Prolog p;
+	
+	public LuconEngine(OutputStream out) {
+		p = new Prolog();
+
+		// Add some listeners for logging/debugging
+		p.addTheoryListener(te -> LOG.debug("New Prolog theory loaded"));
+		p.addExceptionListener(ex -> LOG.error("Exception in Prolog reasoning: " + ex.getMsg()));
+		p.addQueryListener(q -> LOG.debug("Prolog query " + q.getSolveInfo().getQuery().toString()));
+		p.addSpyListener(l -> LOG.trace(l.getMsg() + " " + l.getSource()));
+		p.addOutputListener(l -> { 
+			try {
+				if (out!=null) {
+					out.write(l.getMsg().getBytes());
+				}
+			} catch (Exception e) {
+				LOG.error(e.getMessage(), e);
+			} 
+		});
+	}
+
+	public void setSpy(boolean spy) {
+		p.setSpy(spy);
+	}
+	
+	public void loadPolicy(InputStream is) throws InvalidTheoryException, IOException {
+		// Load policy (= prolog "theory")
+		p.setTheory(new Theory(is));
+	}
+	
+	public List<SolveInfo> query(String query, boolean findAll) throws NoMoreSolutionException, MalformedGoalException, NoSolutionException {
+		List<SolveInfo> result = new ArrayList<>();
+		SolveInfo solution = p.solve(query);
+		System.out.println(solution.getSolution());
+		while (findAll && solution.isSuccess()) {
+			result.add(solution);
+			if (p.hasOpenAlternatives()) {
+				solution = p.solveNext();
+			}
+		}
+		p.solveEnd();
+		return result;
+	}
+	
+	public String getTheory() {
+		return p.getTheory().toString();
+	}
+
+	public String getTheoryAsJSON() {
+		return p.getTheory().toJSON();
+	}
+}
