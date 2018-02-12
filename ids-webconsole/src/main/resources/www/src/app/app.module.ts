39dc8a0e9329cac6774937c1939cac1304e19af8
Lux, Michael <michael.lux@aisec.fraunhofer.de>
2018-02-12 17:44:52 +0100

    removed Http service

--- ids-webconsole/src/main/resources/www/src/app/app.module.ts
+++ ids-webconsole/src/main/resources/www/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { AceEditorModule } from 'ng2-ace-editor';
 
@@ -62,14 +61,13 @@ import { ZoomVizComponent } from './routes/zoom-viz/zoom-viz.component';
     AceEditorModule,
     BrowserModule,
     routing,
-    HttpModule,             // deprecated. To be removed
-    HttpClientModule,       // new since Angular 5
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     FormsModule
 //    ModalModule.forRoot(),
 //    BootstrapModalModule
-    ],
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
