758d61a9327039e04ec6c92d3aa54dee5489f08a
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2018-07-03 10:35:22 +0200

    Correct path for generate_api_docs.sh

--- ids-webconsole/generate_api_docs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run --user 1000 --name ids-api-docs --rm -p 4400:4400 -v ${PWD}/generated/swagger-ui:/opt/public sourcey/spectacle /usr/local/bin/spectacle /opt/public/swagger.json
