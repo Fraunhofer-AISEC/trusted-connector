b7dcc253dd5a49692cc23736cd17d84867bd6bfe
Banse, Christian <christian.banse@aisec.fraunhofer.de>
2018-07-04 18:59:24 +0200

    Converted apidocs script to a yarn command, no extra dep to docker required now

--- ids-webconsole/src/main/resources/www/generate_api_docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "PWD ${PWD}"
-docker run --user 1000 --name ids-api-docs --rm -p 4400:4400 -v ${PWD}/../../../../generated/swagger-ui:/opt/public sourcey/spectacle /usr/local/bin/spectacle /opt/public/swagger.json
