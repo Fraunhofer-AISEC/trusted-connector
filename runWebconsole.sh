3d6103ee3f8d03ca21acdc9335d8007990b6a62d
Lux, Michael <michael.lux@aisec.fraunhofer.de>
2018-07-16 19:37:46 +0200

    fixed Angular dev script

--- runWebconsole.sh
+++ runWebconsole.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 LAST_PWD=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-cd ~/VM_Shared/karaf-policy-platform/ids-webconsole/src/main/resources/www
+cd $DIR/ids-webconsole/src/main/resources/www
 ng serve --progress "$@"
 
 cd $LAST_PWD
