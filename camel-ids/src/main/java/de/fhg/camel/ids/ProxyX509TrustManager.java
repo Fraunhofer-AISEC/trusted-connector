65d01e312728c20ce222854cfd5ea408ad5a5d6c
Michael Lux <michael.lux@aisec.fraunhofer.de>
2018-08-24 16:05:01 +0200

    Implemented local part of certificate binding

--- camel-ids/src/main/java/de/fhg/camel/ids/ProxyX509TrustManager.java
+++ camel-ids/src/main/java/de/fhg/camel/ids/ProxyX509TrustManager.java
@@ -1,6 +1,7 @@
 package de.fhg.camel.ids;
 
 import de.fhg.ids.comm.CertificatePair;
+import org.apache.camel.util.jsse.KeyManagersParameters;
 import org.apache.camel.util.jsse.SSLContextParameters;
 import org.apache.camel.util.jsse.SSLContextServerParameters;
 import org.apache.camel.util.jsse.TrustManagersParameters;
@@ -8,17 +9,40 @@ import org.apache.camel.util.jsse.TrustManagersParameters;
 import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Enumeration;
 
 public class ProxyX509TrustManager implements X509TrustManager {
 
   public static void bindCertificatePair(SSLContextParameters sslContextParameters,
                                          boolean isServer, CertificatePair certificatePair)
       throws GeneralSecurityException, IOException {
-    //TODO: Also retrieve own certificates from keystore
+    // Acquire the local certificate
+    if (sslContextParameters.getKeyManagers() != null) {
+      KeyManagersParameters keyManagers = sslContextParameters.getKeyManagers();
+      if (keyManagers.getKeyStore() != null) {
+        KeyStore keyStore = keyManagers.getKeyStore().createKeyStore();
+        // If a certificate alias is set, use it
+        String alias = sslContextParameters.getCertAlias();
+        // Otherwise, the first alias with a private key is relevant for us
+        if (alias == null) {
+          for (Enumeration<String> es = keyStore.aliases(); es.hasMoreElements(); alias = es.nextElement()) {
+            if (keyStore.isKeyEntry(alias)) {
+              break;
+            }
+          }
+        }
+        // Now assign the certificate from the key store to the pair
+        if (alias != null) {
+          certificatePair.setLocalCertificate(keyStore.getCertificate(alias));
+        }
+      }
+    }
+    // Acquire the remote server certificate OR just configure the server
     if (isServer) {
-      // Configure servers to ask for Client Certificates
+      // SERVER ONLY: Configure server to ask for client certificates
       SSLContextServerParameters serverParameters = sslContextParameters.getServerParameters();
       if (serverParameters == null) {
         serverParameters = new SSLContextServerParameters();
@@ -26,8 +50,7 @@ public class ProxyX509TrustManager implements X509TrustManager {
       serverParameters.setClientAuthentication("WANT");
       sslContextParameters.setServerParameters(serverParameters);
     } else {
-      // CLIENT ONLY! For server side, get certificate from request
-      // Replace X509TrustManager with proxy implementation to log certificates of communication partners
+      // CLIENT ONLY: Replace X509TrustManager with this proxy implementation to log certificate of remote server
       TrustManagersParameters tmParams = sslContextParameters.getTrustManagers();
       X509TrustManager systemTrustManager = (X509TrustManager) tmParams.createTrustManagers()[0];
       tmParams.setTrustManager(new ProxyX509TrustManager(systemTrustManager, certificatePair));
