ba19331b241b76a0d91ee03136cff161a59286ad
Rohit Deshmukh <rohitadeshmukh13@gmail.com>
2019-03-08 19:06:47 +0100

    App Deletion: Added code to invoke the API method wipe on click of the delete button on app-cards
    Signed-off-by: Rohit Deshmukh <rohitadeshmukh13@gmail.com>

--- ids-webconsole/src/main/resources/www/src/app/apps/app-card.component.ts
+++ ids-webconsole/src/main/resources/www/src/app/apps/app-card.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { App } from './app';
 import { PortDef } from './app.port.def';
 import { AppService } from './app.service';
+import { AppsComponent } from './apps.component';
 
 @Component({
     selector: 'app-card',
@@ -14,7 +15,7 @@ export class AppCardComponent implements OnInit {
     statusColor: string;
     private portDefs: Array<PortDef>;
 
-    constructor(private readonly appService: AppService) { }
+    constructor(private readonly appService: AppService, private readonly appsComponent: AppsComponent) { }	// the linter forces to mark 'appsComponent' as readonly which could be misleading; see method 'onDeleteBtnClick' below
 
     get ports(): Array<PortDef> {
         if (!this.portDefs) {
@@ -55,5 +56,12 @@ export class AppCardComponent implements OnInit {
               .subscribe(result => undefined);
             this.app.status = 'Exited(0) 1 seconds ago';
         }
+    }
+
+    onDeleteBtnClick(containerId: string): void {
+      this.appService.wipeApp(containerId)
+        .subscribe(result => undefined);
+      const index = this.appsComponent.apps.indexOf(this.app);
+      this.appsComponent.apps.splice(index, 1) ;
     }
 }
