4fcae4bc52bcd012fef8e9ee6d12bd1f9dc1eda9
Michael Lux <michael.lux@aisec.fraunhofer.de>
2019-06-04 19:21:30 +0200

    Integration of new TPM simulator, continued

--- examples/tpmsim/run.sh
+++ examples/tpmsim/run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Initialize TPM if necessary
+if [ -z "$(ls /swtpm/*.permall 2>/dev/null)" ]; then
+    echo "No .permall file found in /swtpm, initializing TPM..."
+    apt-get update -qq
+    apt-get install -qq gnutls-bin
+    swtpm_setup --tpm2 --tpmstate /swtpm --create-ek-cert
+    apt-get remove --purge gnutls-bin -qq
+fi
+
 # Old, unixsocket-based communication method
 # swtpm socket --tpmstate dir=/swtpm --tpm2 --ctrl type=unixio,path=/swtpm/swtpm-sock &
 
@@ -10,5 +19,16 @@ echo 'Trying to execute "swtpm_ioctl -i --tcp localhost:2322"...'
 until swtpm_ioctl -i --tcp localhost:2322; do sleep 1; done
 echo 'Trying to execute "swtpm_bios --tpm2 --tcp localhost:2321"...'
 until swtpm_bios --tpm2 --tcp localhost:2321; do sleep 1; done
+
 echo 'Starting tpm2d...'
-tpm2d -s
+tpm2d -s &
+# Give tpm2d a little time to come up
+sleep 3
+
+DEV_CERT=/data/cml/tokens/device.cert
+if [ ! -f "$DEV_CERT" ]; then
+    echo "$DEV_CERT not found, running scd..."
+    scd
+    echo "scd finished, copying csr to /swtpm..."
+    cp /data/cml/tokens/device.csr /swtpm
+fi
