7294e6624b06e8dc1e40b040a8aeebf4227b9690
Michael Lux <michael.lux@aisec.fraunhofer.de>
2019-06-19 14:22:10 +0200

    Finished new TPM simulator integration

--- examples/tpmsim/run.sh
+++ examples/tpmsim/run.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 # Initialize tpm2d data directory if not existing
-if [ -d "/data/cml/tpm2d" ]; then
+if [ ! -d "/data/cml/tpm2d" ]; then
     mkdir -p /data/cml/tpm2d
 fi
 # Initialize TPM if not existing
@@ -28,19 +28,36 @@ until swtpm_ioctl -i --tcp localhost:2322; do sleep 1; done
 echo 'Trying to execute "swtpm_bios --tpm2 --tcp localhost:2321"...'
 until swtpm_bios --tpm2 --tcp localhost:2321; do sleep 1; done
 
+# Set environment for openssl_tpm2_engine to use simulator
+export TPM_INTERFACE_TYPE=socsim
+export TPM_SERVER_TYPE=raw
+
 DEV_CERT=/data/cml/tokens/device.cert
 if [ ! -f "$DEV_CERT" ]; then
     echo 'Starting tpm2d...'
     tpm2d -s &
     # Give tpm2d a little time to come up
-    sleep 3
+    sleep 5
     echo "$DEV_CERT not found, running scd..."
+    # Create /dev/tpm0 to workaround a sanity check for a real tpm device in scd
+    touch /dev/tpm0
     if [ ! -f /data/cml/device.conf ]; then
         echo "/data/cml/device.conf not found, initializing with random UUID..."
         echo -e "uuid: \"$(cat /proc/sys/kernel/random/uuid)\"" > /data/cml/device.conf
     fi
-    scd && echo "scd executed successfully." &&
+    n=0
+    until [ $n == 3 ]
+    do
+      scd && break  # substitute your command here
+      echo "Provisioning failed, possibly due to TPM_RC_RETRY. Retrying..."
+      n=$[$n+1]
+    done
+    if [ $n == 3 ]; then
+        echo "scd execution failed $n times, aborting..."
+    else
+        echo "scd executed successfully."
         echo "Replace device.cert with a certificate for device.csr signed of your PKI's CA and restart TPM simulator."
+    fi
 else
     echo 'Starting tpm2d...'
     tpm2d -s
