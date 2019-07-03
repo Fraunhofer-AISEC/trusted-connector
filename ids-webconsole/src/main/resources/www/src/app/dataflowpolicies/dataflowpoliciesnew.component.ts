3bd3dd821aaacb840309c2c19ac0cc60bb0cef4b
Michael Lux <michael.lux@aisec.fraunhofer.de>
2019-07-03 17:10:40 +0200

    Angular import fix

--- ids-webconsole/src/main/resources/www/src/app/dataflowpolicies/dataflowpoliciesnew.component.ts
+++ ids-webconsole/src/main/resources/www/src/app/dataflowpolicies/dataflowpoliciesnew.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AbstractControl } from '@angular/forms/src/model';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 
 import { Policy } from './policy.interface';
