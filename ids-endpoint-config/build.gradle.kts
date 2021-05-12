d4880a06d115f72018cf6f227d48fad674268c6e
Michael Lux <michael.lux@aisec.fraunhofer.de>
2021-05-12 18:57:06 +0200

    Removed irrelevant subproject

--- ids-endpoint-config/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-@Suppress("UNCHECKED_CAST")
-val libraryVersions = rootProject.extra.get("libraryVersions") as Map<String, String>
-
-dependencies {
-    implementation(project(":ids-api")) { isTransitive = false }
-
-    testImplementation("junit", "junit", libraryVersions["junit4"])
-    testImplementation("org.mockito", "mockito-core", libraryVersions["mockito"])
-}
