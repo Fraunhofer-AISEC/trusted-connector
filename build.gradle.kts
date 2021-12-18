e740cb06a329257c1940443c66572d993405154b
Michael Lux <michael.lux@aisec.fraunhofer.de>
2021-12-18 17:12:20 +0100

    Updated Apache Camel, Spring Boot, and some deps, removed invalid code

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     java
 
     // Spring Boot
-    id("org.springframework.boot") version "2.4.5" apply false
+    id("org.springframework.boot") version "2.6.1" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 
     // Other needed plugins
@@ -28,8 +28,8 @@ plugins {
     id("com.google.protobuf") version "0.8.17" apply false
 
     // Kotlin specific
-    kotlin("jvm") version "1.5.20" apply false
-    kotlin("plugin.spring") version "1.5.20" apply false
+    kotlin("jvm") version "1.6.10" apply false
+    kotlin("plugin.spring") version "1.6.10" apply false
 
     id("com.diffplug.spotless") version "5.11.0"
     id("com.github.jk1.dependency-license-report") version "1.16"
--- ids-webconsole/src/main/angular/src/app/routes/routes.component.ts
+++ ids-webconsole/src/main/angular/src/app/routes/routes.component.ts
@@ -1,10 +1,7 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { interval } from 'rxjs';
-import { mergeMap, takeWhile } from 'rxjs/operators';
 
 import { Route } from './route';
-import { RouteMetrics } from './route-metrics';
 import { RouteService } from './route.service';
 
 @Component({
@@ -13,14 +10,11 @@ import { RouteService } from './route.service';
     styleUrls: ['./routes.component.css']
 })
 
-export class RoutesComponent implements OnInit, OnDestroy {
+export class RoutesComponent implements OnInit {
     @Output() public readonly changeTitle = new EventEmitter();
     public title = 'Current Routes';
     public routes: Route[];
     public selectedRoute: Route;
-    public routemetrics: RouteMetrics = new RouteMetrics();
-
-    private alive: boolean;
 
     constructor(private readonly titleService: Title, private readonly routeService: RouteService) {
         this.titleService.setTitle('Message Routes');
@@ -33,18 +27,6 @@ export class RoutesComponent implements OnInit, OnDestroy {
 
     public ngOnInit(): void {
         this.changeTitle.emit('Camel Routes');
-        // Update route metrics every second
-        interval(1000)
-            .pipe(
-                takeWhile(() => this.alive),
-                mergeMap(() => this.routeService.getMetrics())
-            )
-            .subscribe(routeMetrics => this.routemetrics = routeMetrics);
-        this.alive = true;
-    }
-
-    public ngOnDestroy(): void {
-        this.alive = false;
     }
 
     public trackRoutes(index: number, item: Route): string {
